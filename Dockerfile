5a9c8155aadf0e734d41bf4cfd67f10ec91ec702
leocavalcante <lc@leocavalcante.com>
2020-10-10 13:31:23 -0300

    Swoole!!!

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM phpswoole/swoole
+WORKDIR /var/www
+
+ADD composer.json .
+ADD composer.lock .
+RUN composer install --prefer-dist --no-dev --optimize-autoloader
+
+ADD . .
---
+++ server.php
@@ -0,0 +1,23 @@
+#!/usr/bin/env php
+<?php declare(strict_types=1);
+require __DIR__ . '/vendor/autoload.php';
+
+use Cowsayphp\Farm;
+use Swoole\Http\Request;
+use Swoole\Http\Response;
+
+$cow = Farm::create(\Cowsayphp\Farm\Cow::class);
+
+$handler = static function (Request $request, Response $response) use ($cow): void {
+    $text = $request->get['message'] ?? "Set a message by adding ?message=<message here> to the URL";
+    $response->header('Content-Type', 'text/plain');
+    $response->end($cow->say($text));
+};
+
+
+$port = (int)($_ENV['PORT'] ?? 9501);
+echo "Listening on $port\n";
+
+$server = new Swoole\Http\Server('0.0.0.0', $port);
+$server->on('request', $handler);
+$server->start();
